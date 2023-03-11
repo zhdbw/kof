a36ac2d0c7dff6154cb9a73423f4af59b93df4af
zbw <1065346495@qq.com>
2023-03-12 02:00:51 +0800

    完成攻击判定

--- static/js/player/base.js
+++ static/js/player/base.js
@@ -26,9 +26,11 @@ export class Player extends GameObject {
 
         this.pressed_keys = this.root.game_map.controller.pressed_keys;
 
-        this.status = 3; //0: idle, 1: 向前， 2： 向后， 3：跳跃， 4： 攻击， 5： 死亡
+        this.status = 3; //0: idle, 1: 向前， 2： 向后， 3：跳跃， 4： 攻击， 5： 受击， 6： 死亡
         this.animations = new Map();
         this.frame_current_cnt = 0;
+
+        this.hp = 100;
     }
 
     start() { // 初始执行一次
@@ -36,10 +38,8 @@ export class Player extends GameObject {
     }
 
     update_move() {
-        if (this.status === 3) {
-            this.vy += this.gracity;
-        }
 
+        this.vy += this.gracity;
 
         this.x += this.vx * this.timedelta / 1000;
         this.y += this.vy * this.timedelta / 1000;
@@ -47,7 +47,9 @@ export class Player extends GameObject {
         if (this.y > 450) {
             this.y = 450;
             this.vy = 0;
-            this.status = 0;
+            if (this.status == 3) {
+                this.status = 0;
+            }
         }
 
         if (this.x < 0) {
@@ -102,27 +104,92 @@ export class Player extends GameObject {
     }
 
     update_direction() {
+        if (this.status === 6) return; //逝者安息
+
         let players = this.root.players;
         if (players[0] && players[1]) {
             let me = this, you = players[1 - this.id];
             if (me.x < you.x) me.direction = 1;
             else me.direction = -1;
         }
+    }
 
+    id_collision(r1, r2) {
+        if (Math.max(r1.x1, r2.x1) > Math.min(r1.x2, r2.x2))
+            return false;
+        if (Math.max(r1.y1, r2.y1) > Math.min(r1.y2, r2.y2))
+            return false;
+        return true;
+    }
+
+    is_attack() {
+        if (this.status === 6) return; //逝者安息
+
+        this.status = 5;
+        this.frame_current_cnt = 0;
+
+        this.hp = Math.max(this.hp - 10, 0);
+
+        if (this.hp <= 0) {
+            this.status = 6;
+            this.frame_current_cnt = 0;
+        }
+    }
+
+    update_attack() {
+        if (this.status === 4 && this.frame_current_cnt === 18) {
+            let me = this, you = this.root.players[1 - this.id];
+            let r1;
+            if (this.direction > 0) {
+                r1 = {
+                    x1: me.x + 120,
+                    y1: me.y + 40,
+                    x2: me.x + 120 + 100,
+                    y2: me.y + 40 + 20,
+                };
+            } else {
+                r1 = {
+                    x1: me.x + me.width - 120 - 100,
+                    y1: me.y + 40,
+                    x2: me.x + me.width - 120 - 100 + 100,
+                    y2: me.y + 40 + 20,
+                }
+            }
+
+            let r2 = {
+                x1: you.x,
+                y1: you.y,
+                x2: you.x + you.width,
+                y2: you.x + you.height,
+            }
+            if (this.id_collision(r1, r2)) {
+                you.is_attack()
+            }
+        }
     }
 
     update() { // 每一帧执行一次
         this.update_move();
-        console.log(this.status);
+        //console.log(this.status);
         this.update_control();
         this.update_direction();
-        console.log(this.status);
+        this.update_attack();
+        //console.log(this.status);
         this.render();
     }
 
     render() {
-        //this.ctx.fillStyle = this.color;
-        //this.ctx.fillRect(this.x, this.y, this.width, this.height);
+        /*this.ctx.fillStyle = 'blue';
+
+        this.ctx.fillRect(this.x, this.y, this.width, this.height);
+
+        if (this.direction > 0) {
+            this.ctx.fillStyle = 'red';
+            this.ctx.fillRect(this.x + 120, this.y + 40, 100, 20);
+        } else {
+            this.ctx.fillStyle = 'red';
+            this.ctx.fillRect(this.x + this.width - 120 - 100, this.y + 40, 100, 20);
+        }*/
 
         let status = this.status;
 
@@ -152,6 +219,24 @@ export class Player extends GameObject {
 
         if (status === 4 && this.frame_current_cnt === obj.frame_rate * obj.frame_cnt - 1) {
             this.status = 0;
+        }
+
+        /*if (status === 3 && this.frame_current_cnt === obj.frame_rate * (obj.frame_cnt - 6)) {
+            this.frame_current_cnt--;
+            console.log(obj.frame_cnt);
+        }*/
+
+        if (status === 3 && this.frame_current_cnt === obj.frame_rate * (obj.frame_cnt - 1)) {
+            this.frame_current_cnt--;
+            console.log(obj.frame_cnt);
+        }
+
+        if (status === 5 && this.frame_current_cnt === obj.frame_rate * obj.frame_cnt - 1) {
+            this.status = 0;
+        }
+
+        if (status === 6 && this.frame_current_cnt === obj.frame_rate * obj.frame_cnt - 1) {
+            this.frame_current_cnt--;
         }
 
         this.frame_current_cnt++;
